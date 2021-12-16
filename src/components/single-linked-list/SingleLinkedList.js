05fea6b1b77d8aa34c7e692ca7ba70583d7b31b6
ar[arnau321 <arnau123@att.net>
2021-12-16 09:21:55 -0500

    Add sll add function

--- src/components/single-linked-list/SingleLinkedList.js
+++ src/components/single-linked-list/SingleLinkedList.js
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+
+class Node {
+  constructor(value) {
+    this.value = value
+    this.next = null    
+  }
+}
+
+
 
 export default function SingleLinkedList() {
+  const [data, setData] = useState({ head: null })
+  let [size, setSize] = useState(0)
+  const [current, setCurrent] = useState('')
+
+  function insert(value) {
+    let node = new Node(value)
+    console.log(node)
+    if(!data.head){
+      setData({ head: node })
+      // setSize(++size)
+    } else {
+      let current = data.head
+      while(current.next) {
+        current = current.next
+      }
+      current.next = node
+    }
+    setSize(++size)
+    console.log('size', size)
+  }
   return (
     <div>
-      sll
+      <input
+          type="number"
+          name="current"
+          value={current}
+          onChange={e => setCurrent(e.target.value)}
+        />
+      <button
+          disabled={!current || isNaN(current)}
+          onClick={() => {
+            insert(parseInt(current, 10))
+            setCurrent('')
+          }}
+        >
+          Add
+        </button>
     </div>
   )
 }
